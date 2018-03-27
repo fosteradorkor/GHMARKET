abeefb51a77c51d9e5791e1e06b2076109ce46f4
fosteradorkor <fosteradorkor@icloud.com>
2018-03-27 09:56:07 +0000

    settings done
    settings

--- app/src/main/java/com/mirka/app/ghmarket/DB/User.java
+++ app/src/main/java/com/mirka/app/ghmarket/DB/User.java
@@ -11,6 +11,9 @@ public class User extends ParseUser {
 
     private static final String KEY_PROFILE_IMAGE = "profile_image";
     private static final String KEY_USER_NAME = "user_name";
+    private static final String KEY_NOTIFICATION_OFFERS = "notification_offers";
+    private static final String KEY_NOTIFICATION_ALERTS = "notification_alerts";
+    private static final String KEY_NOTIFICATION_FAVORITES = "notification_favorites";
 
     //    name
     public String getName() {
@@ -39,6 +42,26 @@ public class User extends ParseUser {
         super.setEmail(value);
         super.setUsername(value);
     }
+
+    //  notification settings
+    public boolean getNotificationOffers(){
+        return getBoolean(KEY_NOTIFICATION_OFFERS);
+    }
+
+    public void setNotificationOffers(boolean value){put(KEY_NOTIFICATION_OFFERS, value);}
+
+     public boolean getNotificationAlerts(){
+        return getBoolean(KEY_NOTIFICATION_ALERTS);
+    }
+
+    public void setNotificationAlerts(boolean value){put(KEY_NOTIFICATION_ALERTS, value);}
+
+     public boolean getNotificationFavorites(){
+        return getBoolean(KEY_NOTIFICATION_FAVORITES);
+    }
+
+    public void setNotificationFavorites(boolean value){put(KEY_NOTIFICATION_FAVORITES, value);}
+
 
 
     public static User getCurrentUser(){
--- app/src/main/java/com/mirka/app/ghmarket/activities/account/NotificationsActivity.java
+++ app/src/main/java/com/mirka/app/ghmarket/activities/account/NotificationsActivity.java
@@ -1,16 +1,49 @@
 package com.mirka.app.ghmarket.activities.account;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CompoundButton;
 
+import com.mirka.app.ghmarket.DB.User;
 import com.mirka.app.ghmarket.R;
+import com.mirka.app.ghmarket.databinding.ActivitySettingsBinding;
 
-public class NotificationsActivity extends AppCompatActivity {
+public class NotificationsActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+
+    ActivitySettingsBinding layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_settings);
+        layout = DataBindingUtil.setContentView(this, R.layout.activity_settings);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        layout.switcherFavorites.setOnCheckedChangeListener(this);
+        layout.switcherOffers.setOnCheckedChangeListener(this);
+        layout.switcherSales.setOnCheckedChangeListener(this);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        User u = User.getCurrentUser();
+        switch (compoundButton.getId()) {
+            case R.id.switcher_favorites:
+                u.setNotificationFavorites(b);
+                break;
+            case R.id.switcher_offers:
+                u.setNotificationOffers(b);
+                break;
+            case R.id.switcher_sales:
+                u.setNotificationAlerts(b);
+                break;
+        }
+
+        u.saveInBackground();
     }
 }
